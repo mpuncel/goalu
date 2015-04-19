83873c7ca651bc5920bcb202969d1023867d0376
Michael Puncel <mpuncel@gmail.com>
2015-04-18 23:47:40 -0700

    single bit adder

--- component/component.go
+++ component/component.go
@@ -1,22 +1,85 @@
 package component
 
-type orComponent struct {
+type xor struct {
+  in1, in2, out chan bool
+}
+
+func XOR(in1, in2, out chan bool) {
+  x := xor{in1, in2, out}
+
+  go x.start()
+}
+
+func (x *xor) start() {
+  for {
+    val1 := <-x.in1
+    val2 := <-x.in2
+
+    x.out <- val1 != val2
+  }
+}
+
+type or struct {
   in1, in2, out chan bool
 }
 
 func OR(in1, in2, out chan bool) {
-  or := orComponent{in1, in2, out}
+  o := or{in1, in2, out}
 
-  go func() {
-    for {
-      or.do()
-    }
-  }()
+  go o.start()
 }
 
-func (or *orComponent) do() {
-  val1 := <-or.in1
-  val2 := <-or.in2
+func (o *or) start() {
+  for {
+    val1 := <-o.in1
+    val2 := <-o.in2
 
-  or.out <- val1 || val2
+    o.out <- val1 || val2
+  }
+}
+
+type and struct {
+  in1, in2, out chan bool
+}
+
+func AND(in1, in2, out chan bool) {
+  and := and{in1, in2, out}
+
+  go and.start()
+}
+
+func (a *and) start() {
+  for {
+    val1 := <-a.in1
+    val2 := <-a.in2
+
+    a.out <- val1 && val2
+  }
+}
+
+type adder struct {
+  in1, in2, out, carry chan bool
+}
+
+func ADDER(in1, in2, out, carry chan bool) {
+  go func() {
+    for {
+      val1 := <- in1
+      val2 := <- in2
+
+      xor1 := make(chan bool)
+      xor2 := make(chan bool)
+
+      XOR(xor1, xor2, out)
+      xor1 <- val1
+      xor2 <- val2
+
+      and1 := make(chan bool)
+      and2 := make(chan bool)
+      AND(and1, and2, carry)
+
+      and1 <- val1
+      and2 <- val2
+    }
+  }()
 }
--- component/component_test.go
+++ component/component_test.go
@@ -4,6 +4,38 @@ import (
   "testing"
 )
 
+func TestXOR(t *testing.T) {
+  in1 := make(chan bool)
+  in2 := make(chan bool)
+  out := make(chan bool)
+
+  XOR(in1, in2, out)
+
+  in1 <- true
+  in2 <- true
+  if <-out {
+    t.Error("Expected 1 OR 1 to be 0")
+  }
+
+  in1 <- true
+  in2 <- false
+  if !<-out {
+    t.Error("Expected 1 OR 0 to be 1")
+  }
+
+  in1 <- false
+  in2 <- true
+  if !<-out {
+    t.Error("Expected 0 OR 1 to be 1")
+  }
+
+  in1 <- false
+  in2 <- false
+  if <-out {
+    t.Error("Expected 0 OR 0 to be 0")
+  }
+}
+
 func TestOR(t *testing.T) {
   in1 := make(chan bool)
   in2 := make(chan bool)
@@ -14,24 +46,89 @@ func TestOR(t *testing.T) {
   in1 <- true
   in2 <- true
   if !<-out {
-    t.Error("Expected true OR true to be true")
+    t.Error("Expected 1 OR 1 to be 1")
   }
 
   in1 <- true
   in2 <- false
   if !<-out {
-    t.Error("Expected true OR false to be true")
+    t.Error("Expected 1 OR 0 to be 1")
   }
 
   in1 <- false
   in2 <- true
   if !<-out {
-    t.Error("Expected false OR true to be true")
+    t.Error("Expected 0 OR 1 to be 1")
   }
 
   in1 <- false
   in2 <- false
   if <-out {
-    t.Error("Expected false OR false to be false")
+    t.Error("Expected 0 OR 0 to be 0")
+  }
+}
+
+func TestAND(t *testing.T) {
+  in1 := make(chan bool)
+  in2 := make(chan bool)
+  out := make(chan bool)
+
+  AND(in1, in2, out)
+
+  in1 <- true
+  in2 <- true
+  if !<-out {
+    t.Error("Expected 1 AND 1 to be 1")
+  }
+
+  in1 <- true
+  in2 <- false
+  if <-out {
+    t.Error("Expected 1 AND 0 to be 0")
+  }
+
+  in1 <- false
+  in2 <- true
+  if <-out {
+    t.Error("Expected 0 AND 1 to be 0")
+  }
+
+  in1 <- false
+  in2 <- false
+  if <-out {
+    t.Error("Expected 0 AND 0 to be 0")
+  }
+}
+
+func TestADDER(t *testing.T) {
+  in1 := make(chan bool)
+  in2 := make(chan bool)
+  out := make(chan bool)
+  carry := make(chan bool)
+
+  ADDER(in1, in2, out, carry)
+
+  in1 <- true
+  in2 <- true
+  if <- out || !<- carry {
+    t.Error("Expected 1 + 1 to be 10")
+  }
+
+  in1 <- true
+  in2 <- false
+  if !<- out || <- carry {
+    t.Error("Expected 1 + 0 to be 01")
+  }
+
+  in1 <- false
+  in2 <- true
+  if !<- out || <- carry {
+    t.Error("Expected 0 + 1 to be 01")
+  }
+
+  in1 <- false
+  in2 <- false
+  if <- out || <- carry {
+    t.Error("Expected 0 + 0 to be 00")
   }
 }
